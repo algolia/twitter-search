7f7de7a33b73a5bed1e2fe841707a2d1d15356ff
Sylvain UTARD <sylvain.utard@gmail.com>
2013-12-16 09:03:22 +0100

    Keep it secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Twitterhandlesearch::Application.config.secret_key_base = '56203d046c53f5f7d873183167674012e34da592fa32c915f4d9277239447999f59d77f7167d7e7b69573b78d21998758acffead9f53d79f704ac4f9a15fb5af'
+Twitterhandlesearch::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
