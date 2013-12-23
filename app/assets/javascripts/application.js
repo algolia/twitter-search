12024ca82c288c777ec0c3ada8d3d88e6e3ebd15
Sylvain UTARD <sylvain.utard@gmail.com>
2013-12-23 17:41:20 +0100

    Removed debug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -166,7 +166,6 @@ Number.prototype.number_with_delimiter = function(delimiter) {
           if (hit.followers_count > 0) {
             res += hit.followers_count.number_with_delimiter() + ' follower' + (hit.followers_count > 1 ? 's' : '');
           }
-          console.log(hit);
           res += '  </div>' +
             '  <div class="description">' + (hit._highlightResult.description ? hit._highlightResult.description.value : '') + '</div>' +
             '</div>';
